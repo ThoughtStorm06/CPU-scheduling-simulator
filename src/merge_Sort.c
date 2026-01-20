648cb3db78e6a1a8759a988ccf044508c6fda363
ThoughtStorm06 <troze516@gmail.com>
2026-01-20 21:53:06 +0530

    sort updated

--- src/merge_Sort.c
+++ src/merge_Sort.c
@@ -2,34 +2,35 @@
 #include <stdlib.h>
 #include "merge_Sort.h"
 
-// field: 1 - pid, 2 - arrival time , 3 - burst time ,4 - priority   
+// field: 1 - pid, 2 - arrival time, 3 - burst time, 4 - priority
 struct process* sorted_merge(int field, struct process* a, struct process* b) {
     if (!a) return b;
     if (!b) return a;
 
     int compare_a, compare_b;
     if (field == 1) {
-        compare_a = a->pid;
-        compare_b = b->pid;
-    }
-    else if (field == 2) {
-        compare_a = a->arrival_time;
-        compare_b = b->arrival_time;
+        compare_a = a->pid; compare_b = b->pid;
+    } else if (field == 2) {
+        compare_a = a->arrival_time; compare_b = b->arrival_time;
     } else if (field == 3) {
-        compare_a = a->burst_time;
-        compare_b = b->burst_time;
+        compare_a = a->burst_time; compare_b = b->burst_time;
     } else {
-        compare_a = a->priority;
-        compare_b = b->priority;
+        compare_a = a->priority; compare_b = b->priority;
     }
 
+    int a_first = 0;
     if (compare_a < compare_b) {
-        a->next = sorted_merge(field, a->next, b);
-        return a;
+        a_first = 1;
+    } else if (compare_a == compare_b) {
+        // TIE BREAKER: arrange based on process id
+        if (a->pid < b->pid) a_first = 1;
+        else a_first = 0;
+    } else {
+        a_first = 0;
     }
-    else if (compare_a == compare_b) {
-        // If equal,arrange by arrival time
-        a->next = sorted_merge(1, a->next, b);
+
+    if (a_first) {
+        a->next = sorted_merge(field, a->next, b);
         return a;
     } else {
         b->next = sorted_merge(field, a, b->next);
@@ -37,18 +38,14 @@ struct process* sorted_merge(int field, struct process* a, struct process* b) {
     }
 }
 
-static void split_list(struct process* source,
-                       struct process** front_ref,
-                       struct process** back_ref) {
-    if (!source) {
-        *front_ref = NULL;
+static void split_list(struct process* source, struct process** front_ref, struct process** back_ref) {
+    if (!source || !source->next) {
+        *front_ref = source;
         *back_ref = NULL;
         return;
     }
-
     struct process* slow = source;
     struct process* fast = source->next;
-
     while (fast) {
         fast = fast->next;
         if (fast) {
@@ -56,7 +53,6 @@ static void split_list(struct process* source,
             fast = fast->next;
         }
     }
-
     *front_ref = source;
     *back_ref = slow->next;
     slow->next = NULL;
@@ -64,15 +60,10 @@ static void split_list(struct process* source,
 
 void merge_sort(int field, struct process** head_ref) {
     if (!head_ref || !*head_ref || !(*head_ref)->next) return;
-    if (field < 1 || field > 3) return;
-
     struct process* a;
     struct process* b;
-
     split_list(*head_ref, &a, &b);
-
     merge_sort(field, &a);
     merge_sort(field, &b);
-
     *head_ref = sorted_merge(field, a, b);
 }
